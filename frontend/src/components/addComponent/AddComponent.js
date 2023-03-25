e9605b5c5c225a44d9c682fb769da6964e80bb04
kewin.taoanh <kewin.taoanh@gmail.com>
2023-03-25 16:06:24 +0100

    improve UX for add component

--- frontend/src/components/addComponent/AddComponent.js
+++ frontend/src/components/addComponent/AddComponent.js
@@ -1,44 +1,61 @@
 import { Col, Container, Row } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
-import DropdownComponent from "../dropdownComponent/DropdownComponent";
 const AddComponent = () => {
   return (
-    <Container className="my-3">
-      <Row className="align-items-end" xs={1} md={2} lg={5}>
-        <Col className="mt-3">
-          <Form.Group>
-            <Form.Label>Date</Form.Label>
-            <Form.Control type="date" />
-          </Form.Group>
-        </Col>
-        <Col className="mt-3">
-          <Form.Group>
-            <Form.Label>Title</Form.Label>
-            <Form.Control type="text" placeholder="Multisport subscription" />
-          </Form.Group>
-        </Col>
-        <Col className="mt-3">
-          <Form.Group>
-            <Form.Label>Value</Form.Label>
-            <Form.Control type="number" step={0.5} placeholder="100,00" />
-          </Form.Group>
-        </Col>
-        <Col className="mt-3">
-          <Form.Group>
-            <Form.Label>Category</Form.Label>
-            <DropdownComponent />
-          </Form.Group>
-        </Col>
-        <Col className="mt-3">
-          <Form.Group>
-            <Button variant="success" type="submit" className="w-100">
-              Add
-            </Button>
-          </Form.Group>
-        </Col>
-      </Row>
-    </Container>
+    <form>
+      <Container className="my-3">
+        <Row className="align-items-end" xs={1} md={2} lg={5}>
+          <Col className="mt-3">
+            <Form.Group>
+              <Form.Label>Date</Form.Label>
+              <Form.Control required type="date" />
+            </Form.Group>
+          </Col>
+          <Col className="mt-3">
+            <Form.Group>
+              <Form.Label>Title</Form.Label>
+              <Form.Control
+                required
+                type="text"
+                placeholder="Multisport subscription"
+              />
+            </Form.Group>
+          </Col>
+          <Col className="mt-3">
+            <Form.Group>
+              <Form.Label>Value</Form.Label>
+              <Form.Control
+                required
+                type="number"
+                step={0.5}
+                placeholder="100,00"
+              />
+            </Form.Group>
+          </Col>
+          <Col className="mt-3">
+            <Form.Group>
+              <Form.Label>Category</Form.Label>
+              <Form.Select required>
+                <option value="" selected disabled>
+                  Select category
+                </option>
+                <option value="food">Food</option>
+                <option value="transport">Transport</option>
+                <option value="utilities">Utilities</option>
+              </Form.Select>
+            </Form.Group>
+          </Col>
+          <Col className="mt-3">
+            <Form.Group>
+              <Button variant="success" type="submit" className="w-100">
+                Add
+              </Button>
+            </Form.Group>
+          </Col>
+        </Row>
+      </Container>
+    </form>
   );
 };
 
--- frontend/src/components/dropdownComponent/DropdownComponent.js
+++ frontend/src/components/dropdownComponent/DropdownComponent.js
@@ -1,17 +1,15 @@
-import Dropdown from "react-bootstrap/Dropdown";
-import "../dropdownComponent/dropdownComponent.scss";
+import Form from "react-bootstrap/Form";
 
 const DropdownComponent = () => {
   return (
-    <Dropdown>
-      <Dropdown.Toggle variant="outline-dark">Choose category</Dropdown.Toggle>
-
-      <Dropdown.Menu>
-        <Dropdown.Item>Food</Dropdown.Item>
-        <Dropdown.Item>Utilities</Dropdown.Item>
-        <Dropdown.Item>Transport</Dropdown.Item>
-      </Dropdown.Menu>
-    </Dropdown>
+    <Form.Select required>
+      <option value="" selected disabled>
+        Select category
+      </option>
+      <option value="food">Food</option>
+      <option value="transport">Transport</option>
+      <option value="utilities">Utilities</option>
+    </Form.Select>
   );
 };
 
