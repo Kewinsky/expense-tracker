52495b5ca5de0660f7bb963aaa8f80c063ccd83d
kewin.taoanh <kewin.taoanh@gmail.com>
2023-03-25 16:28:41 +0100

    add UI for home page

--- frontend/src/pages/homePage/HomePage.js
+++ frontend/src/pages/homePage/HomePage.js
@@ -1,5 +1,18 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCircleArrowRight } from "@fortawesome/free-solid-svg-icons";
+
 const HomePage = () => {
-  return <div>Home Page</div>;
+  return (
+    <div className="container my-5">
+      <h1>Welcome at Expense_Tracker</h1>
+      <h4>
+        don't hesitate to add new expense{" "}
+        <a href="/tracker">
+          <FontAwesomeIcon icon={faCircleArrowRight} />
+        </a>
+      </h4>
+    </div>
+  );
 };
 
 export default HomePage;
