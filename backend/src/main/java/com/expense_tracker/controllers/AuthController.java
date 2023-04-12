ea6c3fac61e8ed249bf349be21b708a026b782c2
kewin.taoanh <kewin.taoanh@gmail.com>
2023-04-12 16:55:35 +0200

    minor change

--- backend/src/main/java/com/expense_tracker/controllers/AuthController.java
+++ backend/src/main/java/com/expense_tracker/controllers/AuthController.java
@@ -31,7 +31,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
 
-@CrossOrigin(origins = "*", maxAge = 3600)
+@CrossOrigin
 @RestController
 @RequestMapping("/api/auth")
 public class AuthController {
