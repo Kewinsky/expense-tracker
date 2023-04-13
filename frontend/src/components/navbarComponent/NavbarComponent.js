e516b492877391fa2c9cf486331574869fe4bbef
kewin.taoanh <kewin.taoanh@gmail.com>
2023-04-13 09:38:56 +0200

    update navbar items

--- frontend/src/components/navbarComponent/NavbarComponent.js
+++ frontend/src/components/navbarComponent/NavbarComponent.js
@@ -1,9 +1,32 @@
+import { useState, useEffect } from "react";
 import Button from "react-bootstrap/Button";
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
+import AuthService from "../../services/authService";
 
 const NavbarComponent = () => {
+  const [showModeratorBoard, setShowModeratorBoard] = useState(false);
+  const [showAdminBoard, setShowAdminBoard] = useState(false);
+  const [currentUser, setCurrentUser] = useState(undefined);
+
+  useEffect(() => {
+    const user = AuthService.getCurrentUser();
+
+    if (user) {
+      setCurrentUser(user);
+      setShowModeratorBoard(user.roles.includes("ROLE_MODERATOR"));
+      setShowAdminBoard(user.roles.includes("ROLE_ADMIN"));
+    }
+  }, []);
+
+  const logOut = () => {
+    AuthService.logout();
+    setShowModeratorBoard(false);
+    setShowAdminBoard(false);
+    setCurrentUser(undefined);
+  };
+
   return (
     <Navbar bg="dark" variant="dark" expand="lg">
       <Container fluid>
@@ -12,15 +35,31 @@ const NavbarComponent = () => {
         <Navbar.Collapse id="navbarScroll">
           <Nav className="me-auto my-2 my-lg-0" navbarScroll>
             <Nav.Link href="/">Home</Nav.Link>
-            <Nav.Link href="/tracker">Tracker</Nav.Link>
-            <Nav.Link href="/analyzer">Analyzer</Nav.Link>
+            {currentUser && (
+              <>
+                <Nav.Link href="/tracker">Tracker</Nav.Link>
+                <Nav.Link href="/analyzer">Analyzer</Nav.Link>
+              </>
+            )}
+            {showModeratorBoard && (
+              <Nav.Link href="/moderator">Moderator Board</Nav.Link>
+            )}
+            {showAdminBoard && <Nav.Link href="/admin">Admin Board</Nav.Link>}
+            {currentUser && (
+              <Nav.Link href="/profile">
+                {currentUser.username}'s Board
+              </Nav.Link>
+            )}
           </Nav>
-          <Button href="/login" variant="outline-success">
-            Login
-          </Button>
-          {/* <Button href="/logout" variant="outline-danger">
-            Logout
-          </Button> */}
+          {currentUser ? (
+            <Button href="/login" variant="outline-danger" onClick={logOut}>
+              Logout
+            </Button>
+          ) : (
+            <Button href="/login" variant="outline-success">
+              Login
+            </Button>
+          )}
         </Navbar.Collapse>
       </Container>
     </Navbar>
