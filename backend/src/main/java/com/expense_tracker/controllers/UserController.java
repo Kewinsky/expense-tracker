f018ae6fee0ae861dcc95fb2438546a5f66de228
kewin.taoanh <kewin.taoanh@gmail.com>
2023-04-17 09:43:01 +0200

    add new endpoints, add preauthorization

--- backend/src/main/java/com/expense_tracker/controllers/UserController.java
+++ backend/src/main/java/com/expense_tracker/controllers/UserController.java
@@ -1,14 +1,17 @@
 package com.expense_tracker.controllers;
 
+import com.expense_tracker.exceptions.expenses.ExpenseNotFoundException;
 import com.expense_tracker.models.User;
 import com.expense_tracker.exceptions.users.UserNotFoundException;
 import com.expense_tracker.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api")
+@RequestMapping("/api/users")
+@PreAuthorize("hasRole('ADMIN')")
 public class UserController {
 
     @Autowired
@@ -25,13 +28,44 @@ public class UserController {
                 .orElseThrow(() -> new UserNotFoundException(id));
     }
 
-    @GetMapping("/user/me")
-    public Object getCurrentUser() {
-        Object userPrincipal =  SecurityContextHolder.getContext().getAuthentication().getPrincipal();
-        return userPrincipal;
+    @PreAuthorize("hasRole('USER')")
+    @PutMapping("updateUser/{id}")
+    String updateUser(@RequestBody User user,
+                         @PathVariable Long id){
+        return userRepository.findById(id)
+                .map(user1 -> {
+                    user1.setUsername(user.getUsername());
+                    user1.setEmail(user.getEmail());
+                    user1.setPassword(user.getPassword());
+                    userRepository.save(user1);
+                    return "User updated.";
+                })
+                .orElseThrow(() -> new ExpenseNotFoundException(id));
     }
 
+    @PutMapping("updateUserByAdmin/{id}")
+    String updateUserByAdmin(@RequestBody User user,
+                      @PathVariable Long id){
+        return userRepository.findById(id)
+                .map(user1 -> {
+                    user1.setUsername(user.getUsername());
+                    user1.setEmail(user.getEmail());
+                    user1.setPassword(user.getPassword());
+                    user1.setRoles(user.getRoles());
+                    userRepository.save(user1);
+                    return "User updated.";
+                })
+                .orElseThrow(() -> new UserNotFoundException(id));
+    }
 
+    @DeleteMapping("/deleteUser/{id}")
+    String deleteUser(@PathVariable Long id) {
+        if (!userRepository.existsById(id)){
+            throw new UserNotFoundException(id);
+        }
+        userRepository.deleteById(id);
+        return "User with id: " + id + " has been removed.";
+    }
 
 
 }
