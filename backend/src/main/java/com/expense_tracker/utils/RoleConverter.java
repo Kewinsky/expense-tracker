06e4a10ea69f5a0a381e298c209d56863dc5cf59
kewin.taoanh <kewin.taoanh@gmail.com>
2023-04-26 12:22:33 +0200

    change required names for roles

--- backend/src/main/java/com/expense_tracker/utils/RoleConverter.java
+++ backend/src/main/java/com/expense_tracker/utils/RoleConverter.java
@@ -25,13 +25,13 @@ public class RoleConverter {
         } else {
             stringSet.forEach(role -> {
                 switch (role) {
-                    case "admin":
+                    case "ROLE_ADMIN":
                         Role adminRole = roleRepository.findByName(ERole.ROLE_ADMIN)
                                 .orElseThrow(() -> new RuntimeException("Error: Role is not found."));
                         roles.add(adminRole);
 
                         break;
-                    case "mod":
+                    case "ROLE_MODERATOR":
                         Role modRole = roleRepository.findByName(ERole.ROLE_MODERATOR)
                                 .orElseThrow(() -> new RuntimeException("Error: Role is not found."));
                         roles.add(modRole);
