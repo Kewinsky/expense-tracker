c76649f8c17f8e5de3dd146c3d1d9602d89bb416
kewin.taoanh <kewin.taoanh@gmail.com>
2023-04-26 09:27:34 +0200

    handle update user by admin, refactor - extract method

--- backend/src/main/java/com/expense_tracker/controllers/UserController.java
+++ backend/src/main/java/com/expense_tracker/controllers/UserController.java
@@ -3,7 +3,9 @@ package com.expense_tracker.controllers;
 import com.expense_tracker.exceptions.expenses.ExpenseNotFoundException;
 import com.expense_tracker.models.User;
 import com.expense_tracker.exceptions.users.UserNotFoundException;
+import com.expense_tracker.payloads.requests.SignupRequest;
 import com.expense_tracker.repositories.UserRepository;
+import com.expense_tracker.utils.RoleConverter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
@@ -15,6 +17,9 @@ public class UserController {
 
     @Autowired
     private UserRepository userRepository;
+
+    @Autowired
+    private RoleConverter converter;
 
     @GetMapping("/getUsers")
     public Iterable<User> getUsers() {
@@ -31,6 +36,7 @@ public class UserController {
     @PutMapping("updateUser/{id}")
     String updateUser(@RequestBody User user,
                          @PathVariable Long id){
+
         return userRepository.findById(id)
                 .map(user1 -> {
                     user1.setUsername(user.getUsername());
@@ -43,13 +49,13 @@ public class UserController {
     }
 
     @PutMapping("updateUserByAdmin/{id}")
-    String updateUserByAdmin(@RequestBody User user,
+    String updateUserByAdmin(@RequestBody SignupRequest user,
                       @PathVariable Long id){
         return userRepository.findById(id)
                 .map(user1 -> {
                     user1.setUsername(user.getUsername());
                     user1.setEmail(user.getEmail());
-                    user1.setRoles(user.getRoles());
+                    user1.setRoles(converter.toRoleSet(user.getRole()));
                     userRepository.save(user1);
                     return "User updated.";
                 })
---
+++ backend/src/main/java/com/expense_tracker/utils/RoleConverter.java
@@ -0,0 +1,50 @@
+package com.expense_tracker.utils;
+
+import com.expense_tracker.models.Role;
+import com.expense_tracker.models.enums.ERole;
+import com.expense_tracker.repositories.RoleRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Service
+public class RoleConverter {
+
+    @Autowired
+    RoleRepository roleRepository;
+
+    public Set<Role> toRoleSet (Set<String> stringSet) {
+        Set<Role> roles = new HashSet<>();
+
+        if (stringSet == null) {
+            Role userRole = roleRepository.findByName(ERole.ROLE_USER)
+                    .orElseThrow(() -> new RuntimeException("Error: Role is not found."));
+            roles.add(userRole);
+        } else {
+            stringSet.forEach(role -> {
+                switch (role) {
+                    case "admin":
+                        Role adminRole = roleRepository.findByName(ERole.ROLE_ADMIN)
+                                .orElseThrow(() -> new RuntimeException("Error: Role is not found."));
+                        roles.add(adminRole);
+
+                        break;
+                    case "mod":
+                        Role modRole = roleRepository.findByName(ERole.ROLE_MODERATOR)
+                                .orElseThrow(() -> new RuntimeException("Error: Role is not found."));
+                        roles.add(modRole);
+
+                        break;
+                    default:
+                        Role userRole = roleRepository.findByName(ERole.ROLE_USER)
+                                .orElseThrow(() -> new RuntimeException("Error: Role is not found."));
+                        roles.add(userRole);
+                }
+            });
+        }
+
+        return roles;
+    }
+}
