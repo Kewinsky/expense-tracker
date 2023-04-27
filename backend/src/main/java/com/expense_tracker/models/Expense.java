3fcfc9155685b52cf6fb139191d85c9f82f5f340
kewin.taoanh <kewin.taoanh@gmail.com>
2023-04-27 14:02:46 +0200

    change date type

--- backend/src/main/java/com/expense_tracker/models/Expense.java
+++ backend/src/main/java/com/expense_tracker/models/Expense.java
@@ -6,6 +6,7 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
+import java.time.LocalDate;
 
 @Data
 @NoArgsConstructor
@@ -24,7 +25,7 @@ public class Expense {
     @Enumerated(EnumType.STRING)
     private ECategory category;
 
-    private String date;
+    private LocalDate date;
 
     private Long userId;
 }
