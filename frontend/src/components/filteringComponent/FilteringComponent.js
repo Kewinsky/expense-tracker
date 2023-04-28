b2a979528ac2e1d2059e6f9aa015a5cdf2f55347
kewin.taoanh <kewin.taoanh@gmail.com>
2023-04-28 12:34:54 +0200

    add multiselect component and filtering method

---
+++ frontend/src/components/filteringComponent/FilteringComponent.js
@@ -0,0 +1,86 @@
+import { useState } from "react";
+import DropdownComponent from "../dropdownComponent/DropdownComponent";
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
+import { Col, Container, Row } from "react-bootstrap";
+import MultiselectComponent from "../multiselectComponent/MultiselectComponent";
+import "../multiselectComponent/multiselectComponent.scss";
+import { expenseFilter } from "../../helpers/expenseFilter";
+
+const FilteringComponent = ({ expenses, categories, setFilteredExpenses }) => {
+  const [category, setCategory] = useState(["ALL"]);
+  const [month, setMonth] = useState(0);
+
+  const months = ["January", "February", "March", "April", "May"];
+
+  const handleSelectMonth = (e) => {
+    setMonth(e.target.value);
+  };
+
+  const handleSelectCategory = (e) => {
+    setCategory(
+      [].slice.call(e.target.selectedOptions).map((item) => item.value)
+    );
+  };
+
+  const handleFiltering = (e) => {
+    e.preventDefault();
+    const filteredExpenses = expenseFilter(
+      expenses,
+      months.indexOf(month),
+      category
+    );
+    setFilteredExpenses(filteredExpenses);
+  };
+
+  return (
+    <Container className="my-3">
+      <Form onSubmit={handleFiltering}>
+        <Row className="align-items-end" xs={1} md={2} lg={5}>
+          <Col className="mt-3">
+            <Form.Group>
+              <Form.Label>Choose month</Form.Label>
+              <DropdownComponent
+                value={month}
+                onChange={handleSelectMonth}
+                options={months}
+                placeholder={"Select month"}
+              />
+            </Form.Group>
+          </Col>
+          <Col className="mt-3">
+            <Form.Group>
+              <Form.Label>Choose category</Form.Label>
+              {/* <MultiselectComponent options={categories} /> */}
+              <Form.Control
+                as="select"
+                multiple
+                value={category}
+                onChange={handleSelectCategory}
+                className="setHeight"
+              >
+                <option key="ALL" value="ALL" defaultValue>
+                  ALL
+                </option>
+                {categories.map((option) => (
+                  <option key={option} value={option}>
+                    {option}
+                  </option>
+                ))}
+              </Form.Control>
+            </Form.Group>
+          </Col>
+          <Col className="mt-3">
+            <Form.Group>
+              <Button variant="outline-dark" type="submit" className="w-100">
+                Filter
+              </Button>
+            </Form.Group>
+          </Col>
+        </Row>
+      </Form>
+    </Container>
+  );
+};
+
+export default FilteringComponent;
--- frontend/src/pages/trackerPage/TrackerPage.js
+++ frontend/src/pages/trackerPage/TrackerPage.js
@@ -1,11 +1,16 @@
+import { useState } from "react";
 import AddComponent from "../../components/addComponent/AddComponent";
+import FilteringComponent from "../../components/filteringComponent/FilteringComponent";
 import SeparatorComponent from "../../components/separatorComponent/SeparatorComponent";
 import TableComponent from "../../components/tableComponent/TableComponent";
 import ExpenseService from "../../services/expenseService";
 
 const TrackerPage = ({ expenses, setExpenses, currentUser }) => {
   const configLabels = ["date", "title", "value", "category"];
+  const categories = ["FOOD", "TRANSPORT", "UTILITIES"];
   const handleUpdate = "/update/expense";
+
+  const [filteredExpenses, setFilteredExpenses] = useState([]);
 
   const reloadData = async () => {
     const response = await ExpenseService.getExpensesByUser();
@@ -24,13 +29,19 @@ const TrackerPage = ({ expenses, setExpenses, currentUser }) => {
         expenses={expenses}
         setExpenses={setExpenses}
         currentUser={currentUser}
+        categories={categories}
+      />
+      <FilteringComponent
+        expenses={expenses}
+        setFilteredExpenses={setFilteredExpenses}
+        categories={categories}
       />
       <SeparatorComponent />
       <TableComponent
         handleUpdate={handleUpdate}
         handleDelete={handleDelete}
         configLabels={configLabels}
-        records={expenses}
+        records={filteredExpenses}
         setRecords={setExpenses}
       />
     </>
