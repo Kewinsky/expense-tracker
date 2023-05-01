fe5885b756031c3882a6f5c4daaef207a8e5d5dd
kewin.taoanh <kewin.taoanh@gmail.com>
2023-05-01 19:02:41 +0200

    add switch month button

--- frontend/src/pages/analyzerPage/AnalyzerPage.js
+++ frontend/src/pages/analyzerPage/AnalyzerPage.js
@@ -1,5 +1,18 @@
-const AnalyzerPage = ({ currentUser }) => {
-  return <div>Analyzer Page</div>;
+import { useState } from "react";
+import { SwitchMonthComponent } from "../../components/switchComponent/SwitchMonthComponent";
+
+const AnalyzerPage = ({ months }) => {
+  const currentDate = new Date();
+  const [month, setMonth] = useState(0);
+
+  return (
+    <SwitchMonthComponent
+      month={month}
+      setMonth={setMonth}
+      months={months}
+      currentDate={currentDate}
+    />
+  );
 };
 
 export default AnalyzerPage;
