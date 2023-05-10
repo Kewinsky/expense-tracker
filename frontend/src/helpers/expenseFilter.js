311ec21f4be3b7755fa775c8a3cab64ede65c98d
kewin.taoanh <kewin.taoanh@gmail.com>
2023-05-10 11:21:07 +0200

    tracker fix

--- frontend/src/helpers/expenseFilter.js
+++ frontend/src/helpers/expenseFilter.js
@@ -1,6 +1,6 @@
 export const expenseFilter = (items, month, category) => {
   return items.filter((item) => {
-    if (category === null) {
+    if (category === null || !category.length) {
       return new Date(item.date).getMonth() === month;
     }
     return (
