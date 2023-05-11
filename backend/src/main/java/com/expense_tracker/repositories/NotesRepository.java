cfb73675b5dc10b599e327a5171519ff6bac4adb
kewin.taoanh <kewin.taoanh@gmail.com>
2023-05-11 21:11:28 +0200

    create note repo

--- backend/src/main/java/com/expense_tracker/repositories/NotesRepository.java
+++ backend/src/main/java/com/expense_tracker/repositories/NotesRepository.java
@@ -1,6 +1,5 @@
 package com.expense_tracker.repositories;
 
-import com.expense_tracker.models.Expense;
 import com.expense_tracker.models.Note;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -9,5 +8,6 @@ import org.springframework.stereotype.Repository;
 public interface NotesRepository extends JpaRepository<Note, Long> {
 
     Iterable<Note> findByUserId(Long userId);
+    Boolean existsByMonthAndUserId(int month, Long userId);
 
 }
