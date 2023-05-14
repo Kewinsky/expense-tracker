220118f0a9b4ca2f6c3701df3c4ac8aa57f97e76
kewin.taoanh <kewin.taoanh@gmail.com>
2023-05-14 12:15:07 +0200

    handle add/get/update notes on client side

--- frontend/src/components/noteComponent/NoteComponent.js
+++ frontend/src/components/noteComponent/NoteComponent.js
@@ -1,11 +1,15 @@
 import { Form } from "react-bootstrap";
 import "./noteComponent.scss";
 
-const NoteComponent = () => {
+const NoteComponent = ({ notes, setNotes, month }) => {
   return (
     <Form.Group>
       <h4>Notes</h4>
-      <Form.Control as="textarea" />
+      <Form.Control
+        as="textarea"
+        // defaultValue={note}
+        // onChange={handleInputChange}
+      />
     </Form.Group>
   );
 };
