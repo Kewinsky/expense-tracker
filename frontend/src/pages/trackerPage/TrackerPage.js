a47f1864daa830bdfbf31f871cf0f17a281215eb
kewin.taoanh <kewin.taoanh@gmail.com>
2023-05-19 14:19:32 +0200

    add toast to tracker page

--- frontend/src/pages/trackerPage/TrackerPage.js
+++ frontend/src/pages/trackerPage/TrackerPage.js
@@ -1,10 +1,12 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useContext } from "react";
 import AddComponent from "../../components/addComponent/AddComponent";
 import FilteringComponent from "../../components/filteringComponent/FilteringComponent";
 import SeparatorComponent from "../../components/separatorComponent/SeparatorComponent";
 import TableComponent from "../../components/tableComponent/TableComponent";
 import ExpenseService from "../../services/expenseService";
 import { expenseFilter } from "../../helpers/expenseFilter";
+import { toast } from "react-toastify";
+import { ThemeContext } from "../../App";
 
 const TrackerPage = ({
   expenses,
@@ -17,6 +19,8 @@ const TrackerPage = ({
   const handleUpdate = "/update/expense";
   const currentDate = new Date();
 
+  const { theme } = useContext(ThemeContext);
+
   const [category, setCategory] = useState([]);
   const [month, setMonth] = useState(months[currentDate.getMonth()]);
   const [filteredExpenses, setFilteredExpenses] = useState([]);
@@ -26,10 +30,26 @@ const TrackerPage = ({
     setExpenses(response.data);
   };
 
+  const showToastMessageWhenDelete = () => {
+    toast.success("New expense deleted!", {
+      theme: theme,
+    });
+  };
+
+  const showToastErrorMessage = () => {
+    toast.error("Something went wrong!", {
+      theme: theme,
+    });
+  };
+
   const handleDelete = async (id) => {
     await ExpenseService.deleteExpense(id)
       .then(() => reloadData())
-      .catch((err) => console.log(err.response.data));
+      .catch((err) => {
+        showToastErrorMessage();
+        console.log(err.response.data);
+      })
+      .then(() => showToastMessageWhenDelete());
   };
 
   const filterExpenses = () => {
