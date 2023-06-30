2bb37cf0f677e3a47b51b137d00046b2cfada8d1
kewin.taoanh <kewin.taoanh@gmail.com>
2023-06-30 09:59:24 +0200

    descending order of years

--- frontend/src/helpers/yearData.js
+++ frontend/src/helpers/yearData.js
@@ -1,7 +1,7 @@
 export const getYearArray = () => {
   const currentYear = new Date().getFullYear();
   const years = [];
-  for (let year = 1980; year <= currentYear; year++) {
+  for (let year = currentYear; year >= 1980; year--) {
     years.push(year);
   }
   return years;
