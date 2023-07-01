8e83741a7c03f9cbe4368025b9dc6633f65bbf3a
kewin.taoanh <kewin.taoanh@gmail.com>
2023-07-01 22:14:48 +0200

    refactor app.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,37 +3,26 @@ import Router from "./routes/Router";
 import ExpenseService from "./services/expenseService";
 import AuthService from "./services/authService";
 import { ToastContainer } from "react-toastify";
-import { getYearArray } from "./helpers/yearData";
-import { expenseCategories } from "./helpers/expenseCategoriesData";
-import { months } from "./helpers/monthsData";
 
 const ThemeContext = React.createContext();
 
 const App = () => {
-  const years = getYearArray();
-
   const [theme, setTheme] = useState("light");
   const [expenses, setExpenses] = useState([]);
   const [currentUser, setCurrentUser] = useState(undefined);
 
-  const getAllExpenses = async () => {
-    const response = await ExpenseService.getExpensesByUser();
-
-    setExpenses(response.data);
-  };
-
-  const getCurrentUser = () => {
+  const getCurrentUserAndExpenses = async () => {
     const user = AuthService.getCurrentUser();
     if (user) {
       setCurrentUser(user);
+
+      const response = await ExpenseService.getExpensesByUser();
+      setExpenses(response.data);
     }
   };
 
   useEffect(() => {
-    getCurrentUser();
-    if (AuthService.getCurrentUser()) {
-      getAllExpenses();
-    }
+    getCurrentUserAndExpenses();
   }, []);
 
   return (
@@ -44,9 +33,6 @@ const App = () => {
         setExpenses={setExpenses}
         currentUser={AuthService.getCurrentUser()}
         setCurrentUser={setCurrentUser}
-        expenseCategories={expenseCategories}
-        months={months}
-        years={years}
       />
     </ThemeContext.Provider>
   );
