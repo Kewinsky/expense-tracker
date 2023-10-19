f29b22960afc961adde046c3f8805342a2b3efe2
kewin.taoanh <kewin.taoanh@gmail.com>
2023-10-19 14:33:14 +0200

    more clean up

--- frontend/src/components/summaryComponent/SummaryComponent.js
+++ frontend/src/components/summaryComponent/SummaryComponent.js
@@ -1,18 +1,9 @@
 import { BsFillBarChartFill } from "react-icons/bs";
 import "./summaryComponent.scss";
 
-const SummaryComponent = ({
-  outcome,
-  previousOutcome,
-  savings,
-  previousSavings,
-}) => {
+const SummaryComponent = ({ outcome, previousOutcome }) => {
   const outcomeStatus = (current, previous) => {
     return current > previous ? "text-danger" : "text-success";
-  };
-
-  const savingsStatus = (current, previous) => {
-    return current >= previous ? "text-success" : "text-danger";
   };
 
   return (
@@ -28,18 +19,6 @@ const SummaryComponent = ({
           )}
         </p>
         <p className="value-last">{previousOutcome}</p>
-      </div>
-      <div className="mx-3">
-        <h5>Saved</h5>
-        <p className={"value-main " + savingsStatus(savings, previousSavings)}>
-          {savings}
-          {savingsStatus(savings, previousSavings) === "text-danger" ? (
-            <BsFillBarChartFill size={28} className="mx-1 mirrored" />
-          ) : (
-            <BsFillBarChartFill size={28} className="mx-1" />
-          )}
-        </p>
-        <p className="value-last">{previousSavings}</p>
       </div>
     </div>
   );
