e1428dab95feb4faeb3bbf88cf6dabf3b24b292a
Kewinsky <kewin.taoang@gmail.com>
2023-10-21 16:50:22 +0200

    add controller and repository for Income

---
+++ backend/src/main/java/com/expense_tracker/controllers/IncomeController.java
@@ -0,0 +1,13 @@
+package com.expense_tracker.controllers;
+
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@CrossOrigin(origins = "*")
+@RestController
+@RequestMapping("/api/incomes")
+@PreAuthorize("hasRole('USER')")
+public class IncomeController {
+}
