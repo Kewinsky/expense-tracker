f23b537419be1d9b59ede8a8cc7170741c68d9b3
Kewinsky <kewin.taoang@gmail.com>
2023-10-21 17:28:56 +0200

    add implementation for controller

--- backend/src/main/java/com/expense_tracker/repositories/IncomeRepository.java
+++ backend/src/main/java/com/expense_tracker/repositories/IncomeRepository.java
@@ -4,6 +4,12 @@ import com.expense_tracker.models.Income;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDate;
+import java.util.List;
+
 @Repository
 public interface IncomeRepository extends JpaRepository<Income, Long> {
+    List<Income> findByUserIdOrderByDate(Long userId);
+
+    Income findByUserIdAndDateBetween(Long userId, LocalDate startDate, LocalDate endDate);
 }
