8215c0e1fd18c596b61303be525fa691ba660f86
Kewinsky <kewin.taoang@gmail.com>
2023-11-08 23:51:39 +0100

    remove incomes once user has been deleted

--- backend/src/main/java/com/expense_tracker/services/UserService.java
+++ backend/src/main/java/com/expense_tracker/services/UserService.java
@@ -1,9 +1,6 @@
 package com.expense_tracker.services;
 
-import com.expense_tracker.repositories.CategoryRepository;
-import com.expense_tracker.repositories.ExpenseRepository;
-import com.expense_tracker.repositories.NotesRepository;
-import com.expense_tracker.repositories.UserRepository;
+import com.expense_tracker.repositories.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -19,11 +16,17 @@ public class UserService {
     ExpenseRepository expenseRepository;
 
     @Autowired
+    IncomeRepository incomeRepository;
+
+    @Autowired
     NotesRepository notesRepository;
 
     public void deleteUser(Long userId) {
         var expenses = expenseRepository.findByUserIdOrderByDate(userId);
         expenseRepository.deleteAll(expenses);
+
+        var incomes = incomeRepository.findByUserIdOrderByDate(userId);
+        incomeRepository.deleteAll(incomes);
 
         var categories = categoryRepository.findByUserId(userId);
         categoryRepository.deleteAll(categories);
