80b069364c0fd4ab76d99be209f17ae7f69f3980
kewin.taoanh <kewin.taoanh@gmail.com>
2023-12-08 13:37:03 +0100

    update notes endpoint

---
+++ backend/src/main/java/com/expense_tracker/services/NoteService.java
@@ -0,0 +1,45 @@
+package com.expense_tracker.services;
+
+import com.expense_tracker.exceptions.notes.NoteNotFoundException;
+import com.expense_tracker.exceptions.users.UserNotFoundException;
+import com.expense_tracker.models.Note;
+import com.expense_tracker.repositories.NoteRepository;
+import com.expense_tracker.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Objects;
+
+@Service
+public class NoteService {
+    @Autowired
+    private NoteRepository noteRepository;
+
+    @Autowired
+    private UserRepository userRepository;
+
+
+    public List<Note> getNotes(Long userId) {
+        userRepository.findById(userId)
+                .orElseThrow(() -> new UserNotFoundException(userId));
+
+        return noteRepository.findByUserId(userId);
+    }
+
+    public String updateNote(Long noteId, Note updateNote) {
+        if (!Objects.equals(noteId, "undefined")) {
+            return noteRepository.findById(noteId)
+                    .map(nt -> {
+                        nt.setNote(updateNote.getNote());
+                        noteRepository.save(nt);
+                        return "Note updated successfully";
+                    })
+                    .orElseThrow(() -> new NoteNotFoundException(noteId));
+        }
+
+        noteRepository.save(updateNote);
+
+        return "Note added successfully";
+    }
+}
