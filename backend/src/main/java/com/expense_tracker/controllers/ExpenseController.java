61ebe3dcaf226243d900e4466cf3695bd744a620
kewin.taoanh <kewin.taoanh@gmail.com>
2023-12-08 13:35:11 +0100

    update expenses endpoint

---
+++ backend/src/main/java/com/expense_tracker/controllers/ExpenseController.java
@@ -0,0 +1,51 @@
+package com.expense_tracker.controllers;
+
+import com.expense_tracker.models.Expense;
+import com.expense_tracker.payloads.responses.ExpenseResponse;
+import com.expense_tracker.services.ExpenseService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@CrossOrigin(origins = "*")
+@RestController
+@RequestMapping("/api/expenses")
+@PreAuthorize("hasRole('USER')")
+public class ExpenseController {
+
+    @Autowired
+    ExpenseService expenseService;
+
+    @GetMapping("/getExpensesByUser/{id}")
+    @ResponseBody
+    ResponseEntity<List<ExpenseResponse>> getExpensesByUser(@PathVariable Long id) {
+        var result = expenseService.getExpensesWithCategory(id);
+
+        return ResponseEntity.ok(result);
+    }
+
+    @PostMapping("/addExpense")
+    ResponseEntity<String> addExpense(@RequestBody Expense expense) {
+        expenseService.addExpense(expense);
+
+        return ResponseEntity.ok("Expense added successfully");
+    }
+
+    @PutMapping("updateExpense/{id}")
+    ResponseEntity<String> updateExpense(@RequestBody Expense expense,
+                                         @PathVariable Long id) {
+        var result = expenseService.updateExpense(id, expense);
+
+        return ResponseEntity.ok(result);
+    }
+
+    @DeleteMapping("/deleteExpense/{id}")
+    ResponseEntity<String> deleteExpense(@PathVariable Long id) {
+        expenseService.deleteExpense(id);
+
+        return ResponseEntity.ok("Expense deleted successfully");
+    }
+}
--- backend/src/main/java/com/expense_tracker/services/ExpenseService.java
+++ backend/src/main/java/com/expense_tracker/services/ExpenseService.java
@@ -1,9 +1,12 @@
 package com.expense_tracker.services;
 
+import com.expense_tracker.exceptions.expenses.ExpenseNotFoundException;
+import com.expense_tracker.exceptions.users.UserNotFoundException;
 import com.expense_tracker.models.Expense;
 import com.expense_tracker.payloads.responses.ExpenseResponse;
 import com.expense_tracker.repositories.CategoryRepository;
 import com.expense_tracker.repositories.ExpenseRepository;
+import com.expense_tracker.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -14,6 +17,9 @@ import java.util.List;
 public class ExpenseService {
     @Autowired
     CategoryRepository categoryRepository;
+
+    @Autowired
+    private UserRepository userRepository;
 
     @Autowired
     private ExpenseRepository expenseRepository;
@@ -37,5 +43,34 @@ public class ExpenseService {
         }
 
         return expensesWithCategory;
+    }
+
+    public void addExpense(Expense expense) {
+        var userId = expense.getUser().getId();
+
+        userRepository.findById(userId)
+                .orElseThrow(() -> new UserNotFoundException(userId));
+
+        expenseRepository.save(expense);
+    }
+
+    public String updateExpense(Long id, Expense updateExpense) {
+        return expenseRepository.findById(id)
+                .map(exp -> {
+                    exp.setTitle(updateExpense.getTitle());
+                    exp.setValue(updateExpense.getValue());
+                    exp.setCategory(updateExpense.getCategory());
+                    exp.setDate(updateExpense.getDate());
+                    expenseRepository.save(exp);
+                    return "Expense updated successfully";
+                })
+                .orElseThrow(() -> new ExpenseNotFoundException(id));
+    }
+
+    public void deleteExpense(Long expenseId) {
+        if (!expenseRepository.existsById(expenseId)) {
+            throw new ExpenseNotFoundException(expenseId);
+        }
+        expenseRepository.deleteById(expenseId);
     }
 }
