550cbdf39784b674c4c303879dc3d922bbf78bdf
Kewin Tao Anh <kewin.taoanh@gmail.com>
2023-12-08 10:55:50 +0100

    refactor category controller

--- backend/src/main/java/com/expense_tracker/controllers/CategoryController.java
+++ backend/src/main/java/com/expense_tracker/controllers/CategoryController.java
@@ -1,10 +1,10 @@
 package com.expense_tracker.controllers;
 
-import com.expense_tracker.exceptions.categories.CategoryNotFoundException;
 import com.expense_tracker.models.Category;
 import com.expense_tracker.repositories.CategoryRepository;
 import com.expense_tracker.services.CategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
@@ -27,35 +27,28 @@ public class CategoryController {
     @GetMapping("/getCategoriesByUser/{id}")
     @ResponseBody
     List<Category> getCategoriesByUser(@PathVariable Long id) {
-        userController.getUserById(id);
-
-        return categoryRepository.findByUserId(id);
+        return categoryService.getCategories(id);
     }
 
-    @PostMapping("/addCategory")
-    String addCategory(@RequestBody Category category) {
-        userController.getUserById(category.getUser().getId());
-        categoryRepository.save(category);
 
-        return "Category added successfully";
+    @PostMapping("/addCategory")
+    ResponseEntity<String> addCategory(@RequestBody Category category) {
+        categoryService.addCategory(category);
+
+        return ResponseEntity.ok("Category added successfully");
     }
 
     @PutMapping("updateCategory/{id}")
-    String updateCategory(@RequestBody Category category,
-                          @PathVariable Long id) {
-        return categoryRepository.findById(id)
-                .map(cat -> {
-                    cat.setTitle(category.getTitle());
-                    categoryRepository.save(cat);
-                    return "Category updated successfully";
-                })
-                .orElseThrow(() -> new CategoryNotFoundException(id));
+    public ResponseEntity<String> updateCategory(@RequestBody Category category, @PathVariable Long id) {
+        var result = categoryService.updateCategory(id, category);
+
+        return ResponseEntity.ok(result);
     }
 
     @DeleteMapping("/deleteCategory/{id}")
-    String deleteCategory(@PathVariable Long id) {
+    ResponseEntity<String> deleteCategory(@PathVariable Long id) {
         categoryService.deleteCategory(id);
 
-        return "Category deleted successfully";
+        return ResponseEntity.ok("Category deleted successfully");
     }
 }
--- backend/src/main/java/com/expense_tracker/services/CategoryService.java
+++ backend/src/main/java/com/expense_tracker/services/CategoryService.java
@@ -2,10 +2,15 @@ package com.expense_tracker.services;
 
 import com.expense_tracker.exceptions.categories.CategoryCannotBeDeletedException;
 import com.expense_tracker.exceptions.categories.CategoryNotFoundException;
+import com.expense_tracker.exceptions.users.UserNotFoundException;
+import com.expense_tracker.models.Category;
 import com.expense_tracker.repositories.CategoryRepository;
 import com.expense_tracker.repositories.ExpenseRepository;
+import com.expense_tracker.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class CategoryService {
@@ -13,7 +18,36 @@ public class CategoryService {
     private CategoryRepository categoryRepository;
 
     @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
     private ExpenseRepository expenseRepository;
+
+    public List<Category> getCategories(Long id) {
+        userRepository.findById(id)
+                .orElseThrow(() -> new UserNotFoundException(id));
+
+        return categoryRepository.findByUserId(id);
+    }
+
+    public void addCategory(Category category) {
+        var userId = category.getUser().getId();
+
+        userRepository.findById(userId)
+                .orElseThrow(() -> new UserNotFoundException(userId));
+
+        categoryRepository.save(category);
+    }
+
+    public String updateCategory(Long id, Category updatedCategory) {
+        return categoryRepository.findById(id)
+                .map(existingCategory -> {
+                    existingCategory.setTitle(updatedCategory.getTitle());
+                    categoryRepository.save(existingCategory);
+                    return "Category updated successfully";
+                })
+                .orElseThrow(() -> new CategoryNotFoundException(id));
+    }
 
     public void deleteCategory(Long categoryId) {
         var category = categoryRepository.findById(categoryId)
